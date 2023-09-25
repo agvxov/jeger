c96567dc44377565be4a2b127361f2973eec00c9
anon <anon@anon.anon>
2023-09-25 15:47:18 +0200

    fixed spelling mistake

--- source/test.hpp
+++ source/test.hpp
@@ -25,7 +25,7 @@ void print_leader(const bool passed, const int n) {
 	if (passed) {
 		printf("\033[32;1mSuccess\033[0m.  %02d\033[1m - \033[0m", n);
 	} else {
-		printf("\033[31;1mFailiour\033[0m. %02d\033[1m - \033[0m", n);
+		printf("\033[31;1mFailure\033[0m. %02d\033[1m - \033[0m", n);
 	}
 }
 
